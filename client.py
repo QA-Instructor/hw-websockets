63f52880d6b0a6325504c05ada6bc94282595ca7
stream2stream <swright@celestial.co.uk>
2020-08-19 15:31:36 +0100

    broken down code into smaller steps, and added a termination event

--- client.py
+++ client.py
@@ -3,8 +3,11 @@ import websockets
 
 async def hello(uri):
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello world!")
-        await websocket.recv()
+        for x in range(10):
+            await websocket.send("Hello world!")
+            resp = await websocket.recv()
+            print( resp )
+        await websocket.send("quit")
 
 asyncio.get_event_loop().run_until_complete(
     hello('ws://localhost:8765'))
--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 import asyncio
 import websockets
 
+# I've broken the code down into small steps so that each step can be expained carefully
+
+# get the event loop so that you stop it gracefully
+eventLoop = asyncio.get_event_loop()
+
 async def echo(websocket, path):
     async for message in websocket:
         print( message )
         await websocket.send(message)
+        # if the client sends a character string "quit" then stop the event loop
+        if message == "quit" :
+            eventLoop.stop()
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
-asyncio.get_event_loop().run_forever()
+print("waiting for data...")
+wsFuture = websockets.serve(echo, 'localhost', 8765) 
+# schedule echo() to run
+eventLoop.run_until_complete( wsFuture )
 
-#if __name__ == '__main__':    
+# run all waiting tasks
+eventLoop.run_forever()
+print("run forever terminated")
+
 
